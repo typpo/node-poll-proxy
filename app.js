304b12f66f92dc02bfa7bc4d6bffb12f00c05e3f
typpo <ian@typpo.us>
2011-12-20 01:15:32 -0800

    More work, using dnode

--- app.js
+++ app.js
@@ -1,16 +1,80 @@
-var app = require('express').createServer()
+var express = require('express')
+    , app = express.createServer()
     , io = require('socket.io').listen(app)
     , url = require('url')
     , http = require('http')
+    , dnode = require('dnode')
 
 app.listen(8080);
 
+// Static directory setup
+app.configure('development', function(){
+  app.use(express.static(__dirname + '/client'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+// Dnode setup
+var server = dnode({
+  get: function (data, predicate, cb) {
+         console.log(predicate.toString());
+    u = url.parse(data.url);
+    //TODO these options:
+    //    interval: 500,
+    //    backoff: false,
+    //    max: 40,
+    var options = {
+      host: u.host,
+      port: 80,
+      path: u.pathname + u.search,
+    };
+    console.log('get ' + u.host);
+    console.log(u.pathname);
+
+    http.get(options, function(resp){
+      var chunks = [];
+      resp.on('data', function(chunk){
+        // TODO translate relative paths?
+        chunks.push(chunk);
+      }).on('end', function() {
+        var obj;
+        try {
+          obj  = JSON.parse(chunks.join(''));
+        }
+        catch (e) {
+          console.log('Failed parsing json');
+          cb(false);
+          return;
+        }
+        if (predicate(obj)) {
+          console.log('write on ' + data.event);
+          cb(true, obj);
+        }
+        else {
+          console.log('suppressing response due to no predicate match');
+        }
+      });
+    }).on('error', function(e){
+      console.log("Got error: " + e.message);
+    })
+  }
+});
+server.listen(app);
+
+
+/*
 io.sockets.on('connection', function(socket) {
   // data:
   // - url: url to request
   // - channel: event to trigger on client
   // fn: predicate that says whether or not we want to pass on poll response
-  socket.on('get', function(data, fn) {
+  socket.on('get', function(data) {
+
+    console.log('X');
+    console.log(data);
+    console.log(data());
+    console.log('X');
+    return;
+
     u = url.parse(data.url);
     //TODO these options:
 //    interval: 500,
@@ -19,17 +83,30 @@ io.sockets.on('connection', function(socket) {
     var options = {
       host: u.host,
       port: 80,
-      path: u.pathname,
+      path: u.pathname + u.search,
     };
+    console.log('get ' + u.host);
+    console.log(u.pathname);
+
     http.get(options, function(resp){
       var chunks = [];
       resp.on('data', function(chunk){
         // TODO translate relative paths?
         chunks.push(chunk);
       }).on('end', function() {
-        var obj = JSON.parse(chunks.join(''));
-        if (fn(obj)) {
-          socket.emit(data.event, true, obj);
+        var obj;
+        try {
+          obj  = JSON.parse(chunks.join(''));
+        }
+        catch (e) {
+          console.log('Failed parsing json');
+          socket.emit(data.event, false);
+          return;
+        }
+        console.log(data.fn);
+        if (data.fn(obj)) {
+          //socket.emit(data.event, true, obj);
+          console.log('write on ' + data.event);
         }
       });
     }).on('error', function(e){
@@ -59,3 +136,4 @@ app.get('/*', function(req, res) {
   })
 });
 
+*/
