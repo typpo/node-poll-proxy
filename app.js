4949f87c7c9ce8c2258e6c966e5e7dfe3c21d46b
typpo <ian@typpo.us>
2011-12-30 11:59:01 -0500

    repeat polls

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ everyone.now.logStuff = function(msg){
     console.log(msg);
 }
 
-everyone.now.get = function(data, predicate, cb) {
+everyone.now.get = function(data, poll_status_check, cb) {
   var u = url.parse(data.url);
-  //TODO these options:
+  //TODO add these options:
   //    interval: 500,
   //    backoff: false,
   //    max: 40,
@@ -24,33 +24,37 @@ everyone.now.get = function(data, predicate, cb) {
   console.log('get ' + u.host);
   console.log(options.path);
 
-  http.get(options, function(resp){
-    var chunks = [];
-    resp.on('data', function(chunk){
-      // TODO translate relative paths?
-      chunks.push(chunk);
-    }).on('end', function() {
-      var obj;
-      try {
-        obj = JSON.parse(chunks.join(''));
-      }
-      catch (e) {
-        console.log('Failed parsing json');
-        cb(false);
-        return;
-      }
+  var repeat = true;
+  while (repeat) {
+    http.get(options, function(resp){
+      var chunks = [];
+      resp.on('data', function(chunk){
+        chunks.push(chunk);
+      }).on('end', function() {
+        var obj;
+        try {
+          obj = JSON.parse(chunks.join(''));
+        }
+        catch (e) {
+          console.log('Failed parsing json');
+          cb(false);
+          return;
+        }
 
-      predicate(obj, function(status) {
-        // if polling is complete
-        console.log('poll results complete');
-        cb(true, obj);
+        poll_status_check(obj, function(status) {
+          if (status) {
+            // polling is complete
+            repeat = false;
+            console.log('poll results complete');
+            cb(true, obj);
+          }
+        });
       });
-    });
-  }).on('error', function(e){
-    console.log("Got error: " + e.message);
-  }); // end http get
+    }).on('error', function(e){
+      console.log("Got error: " + e.message);
+    }); // end http get
+  }
 } // end everyone.now.get
 
 server.use(express.static(__dirname + '/client'));
-
 server.listen(8080);
